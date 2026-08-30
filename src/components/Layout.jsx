5f802de6ef9f7049e3ba3141dc256fc9a8960fcd
fadhies <fadhies@gmail.com>
2026-08-30 21:13:04 +0000

    Visual edits

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -65,7 +65,7 @@ export default function Layout({ children }) {
             </div> :
 
           <Link to="/" className="flex items-center gap-2">
-              <div className="w-9 h-9 rounded-xl bg-primary flex items-center justify-center shadow-md shadow-primary/30">
+              <div className="w-9 h-9 rounded-xl flex items-center justify-center shadow-md shadow-primary/30 bg-[#de02d7]">
                 <Bike className="w-5 h-5 text-primary-foreground" />
               </div>
               <span className="tracking-tight [font-family:'Alegreya',_serif] font-medium text-2xl">
